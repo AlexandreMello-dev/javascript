b12a6633fa17434ab419e932220c9f7d55d6f071
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-07 15:23:07 -0300

    criando codigos JavaScript

---
+++ JavaScript-tipos-variaveis-funçães/type-boolean.js
@@ -0,0 +1,13 @@
+//boolean
+//true -> verdadeira
+//false -> falso
+
+const num1 = 5;
+const num2 = 5;
+
+//console.log(num1 === num2);
+
+const texto1 = "Alura";
+let texto2 = "alura";
+
+console.log(texto1 === texto2);
---
+++ JavaScript-tipos-variaveis-funçães/type-string.js
@@ -0,0 +1,10 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura123"
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+//console.log(citacao)
+
+// concatenação (+)
+
