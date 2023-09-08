0cb4323f2e6216a9d200123c65a36a8aeacb17fc
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-08 19:49:46 -0300

    codigos var e conversões

---
+++ JavaScript-tipos-variaveis-funçães/truthy-falsy.js
@@ -0,0 +1,22 @@
+//boolean
+
+const contapaga = true;
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false)
+// console.log('' == false)
+// console.log(1 == true)
+
+//null ==> vazio ou nada
+//undefined
+
+let minhavar; 
+let varNull = null;
+
+let numero = 3;
+let texto = 'Alura'
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
