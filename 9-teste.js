e12ea3930bd85daf7396355c60dc7bffb6f7ede1
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-08 20:34:13 -0300

    converti números

--- 9-teste.js
+++ 9-teste.js
@@ -1,4 +1,17 @@
-const soma = 10 + 10;
-const multiplicacao = 10 * 10;
-const operacao = (soma + multiplicacao) + 10;
-console.log(operacao)
+// const soma = 10 + 10;
+// const multiplicacao = 10 * 10;
+// const operacao = (soma + multiplicacao) + 10;
+// console.log(operacao)
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
--- JavaScript-tipos-variaveis-funçães/conversoes.js
+++ JavaScript-tipos-variaveis-funçães/conversoes.js
@@ -3,12 +3,25 @@
 
 // conversão implícita
 
-const numero = 456;
-const numeroString = Number('456');
+let numero = 456;
+let numeroString = Number('456');
 
 // Number()
 // String()
 
-console.log(numero + numeroString);
+// console.log(numero + numeroString);
+
+// console.log("Seu numero é " + numero)
 
 // conversão explícita
+
+// var telefone = 12341234;
+// console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+
+// let telefone = 12341234;
+// console.log("O telefone é " + telefone.toString()); // o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+
+let usuarioConectado = false;
+console.log(String(usuarioConectado)); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+usuarioConectado = true;
+console.log(String(usuarioConectado)); // agora teremos uma string “true”.
