886e4234ec3d383ef49fdeae6a8c0252545fa15f
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-11 17:32:23 -0300

    comparações, template-string e ternário

---
+++ JavaScript-tipos-variaveis-funçães/template-string.js
@@ -0,0 +1,4 @@
+const nome = "Alexandre";
+const idade = 22;
+const cidadeDeNascimento = "Passo Fundo";
+console.log(`meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`);
