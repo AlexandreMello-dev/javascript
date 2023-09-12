0f9aa3977fc665ba5384a7d2572760cbbd3ee2fe
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-12 20:34:48 -0300

    criados e treinado function, arrows e espressões

---
+++ JavaScript-tipos-variaveis-funçães/parametros.js
@@ -0,0 +1,30 @@
+// parâmetros de função
+
+// function soma(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(soma(2, 2))
+// console.log(soma(4, 4))
+// console.log(soma(3, 3))
+// console.log(soma(9, 9))
+
+// ordem dos parâmetros
+
+function nomeIdade(nome, idade){
+    return `meu nome é ${nome} e minha idade é ${idade}`;
+}
+
+// console.log(nomeIdade("alexandre", 22))
+
+function soma(num1, num2){
+    return num1 + num2;
+}
+
+function multiplica(num1=1 , num2=1){''
+    return num1 * num2;
+}
+
+console.log(multiplica(soma(4, 5), soma(3,3)))
+
+
