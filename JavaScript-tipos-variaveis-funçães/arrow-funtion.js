f5d5d9e3aca14dcc2639b8c8af12b3aca78e2953
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-12 20:53:06 -0300

    mais arrows

--- JavaScript-tipos-variaveis-funçães/arrow-funtion.js
+++ JavaScript-tipos-variaveis-funçães/arrow-funtion.js
@@ -8,12 +8,12 @@ const soma = (num1, num2) => num1 + num2;
 
 //Arrow function com + de 1 linha de instrução
 
-const somaNumerosPequenos = {num1, num2} => {
+
+const somaNumerosPequenos = (num1, num2) => {
     if (num1 > 10 || num2 > 10) {
         return "somente números de 1 a 9"
     } else {
         return num1 + num2;
     }
 }
-
-// operador maior ou igual que ">="
+console.log(somaNumerosPequenos(2, 2));
