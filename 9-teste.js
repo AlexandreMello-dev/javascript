bd1216da5d6183b475972fad927ff5627b9d97d8
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-13 16:05:25 -0300

    testes e treinando

--- 9-teste.js
+++ 9-teste.js
@@ -1,23 +1,5 @@
-let senha;
+const num = 10;
 
-senhas = new Array (
-
-    'minhanovasenha',
-    'eusoualexandre',
-    'MEUNOVOMUNDO',
-    'minhasenha',
-)
-
-let cont = 0;
-
-while (cont <= 5 ) {
-    if (senhas == 'MEUNOVOMUNDO'){
-        console.log(`A achamos a sua senha no indice ${cont}`);
-        break
-    } else {
-        cont += 1
-        console.log("Achamos sua senha")
-        
-    }
- 
+if (num) {
+    console.log(`o número é ${num}`);
 }
--- JavaScript-tipos-variaveis-funçães/arrow-funtion.js
+++ JavaScript-tipos-variaveis-funçães/arrow-funtion.js
@@ -16,4 +16,5 @@ const somaNumerosPequenos = (num1, num2) => {
         return num1 + num2;
     }
 }
+
 console.log(somaNumerosPequenos(2, 2));
