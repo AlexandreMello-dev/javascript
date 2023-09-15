b25c78e2c7781c29d901c3338e106d3b3d5b154f
AlexandreMello-dev <alexandremellodev@gmail.com>
2023-09-15 18:39:08 -0300

    novos modulos usando javascript

--- javaScript-programando-a-Orientação-a-Objetos/index.js
+++ javaScript-programando-a-Orientação-a-Objetos/index.js
@@ -1,23 +1,24 @@
-class Cliente{
-    nome;
-    cpf;
-    agencia;
-    saldo;
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia= 1001;
 
-cliente2.saldo = 0;
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.agencia = 1001;
 
-console.log(cliente1, cliente2);
+
+ContaCorrenteRicardo.depositar(100);
+ContaCorrenteRicardo.depositar(100);
+ContaCorrenteRicardo.depositar(100);
+
+
+const valorSacado = ContaCorrenteRicardo.sacar(50);
+console.log(valorSacado)
+
+console.log(ContaCorrenteRicardo)
