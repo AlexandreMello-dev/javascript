8935345b1dcb4850bc1aac994e210cd436175035
AlexandreMello-dev <122244229+AlexandreMello-dev@users.noreply.github.com>
2024-09-10 23:24:14 -0300

    exercício de tabuada

---
+++ Exercícios_de_repetição/tabuada.js
@@ -0,0 +1,4 @@
+var num = 2
+for (var c = 1; c <= 10; c++){
+    console.log(`${c} X ${num} = ${c * num}`)
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log("Vai começa!")
+for (var c = 1;c <= 10; c++){
+    console.log(`${c}`)
+}
+console.log("Fim!")
